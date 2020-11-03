75053414ed4664573175c37f6d1023661b9ef0b3
Paul Cortez <cortezpaul19@gmail.com>
2020-11-03 17:37:48 -0600

    Added Acronym Generator

---
+++ acronymGenerator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace acronymGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            
+            Console.WriteLine("Enter strings to convert to acronym");
+            string acronym = Console.ReadLine();
+            string[] acronym2 = acronym.Split(' ');
+            
+            foreach(string i in acronym2){
+                string capitalLetter = i.ToUpper();
+                Console.Write(capitalLetter[0]);
+            }
+            
+        }
+    }
+}
