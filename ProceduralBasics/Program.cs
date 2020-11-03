5609940b0741807e6b127b4c14376e3ead6a857e
Paul Cortez <cortezpaul19@gmail.com>
2020-11-03 15:15:52 -0600

    Created a Triangle with User Input

--- ProceduralBasics/Program.cs
+++ ProceduralBasics/Program.cs
@@ -6,7 +6,21 @@ namespace ProceduralBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            for (int i = 0; i < number; i++)
+            {
+                // each iteration of this loop prints one whole line.
+                for (int j = 0; j < i + 1; j++)
+                {
+                    // each iteration of this loop just prints one character
+                    Console.Write("#");
+                }
+                Console.WriteLine(); // just a line break
+                // i could also just have printed this, without the inner loop:
+                
+            }
         }
     }
 }
